96f4444e298d8c8cb9ac373c666c192bd734b9ac
BinxMyBeloved <159493132+BinxMyBeloved@users.noreply.github.com>
2024-02-09 10:06:11 +1100

    added import files

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,16 +1,15 @@
 extends KinematicBody2D
-
-
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
-
+var bulletSource = preload ("res://Bullet/Bullet.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	set_process(true)
+	pass # replace with function body
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func _process(delta):
+	if Input.is_action_just_pressed("fire"):
+		var bulletInstance = bulletSource.instance()
+		bulletInstance.position = Vector2(position.x, position.y-45)
+		get_tree().get_root().add_child(bulletInstance)
