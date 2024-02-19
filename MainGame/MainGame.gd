102ae67ae860114abc4765db5835fc7e0f563e39
BinxMyBeloved <159493132+BinxMyBeloved@users.noreply.github.com>
2024-02-19 13:07:24 +1100

    working on timer

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -7,8 +7,12 @@ var currentTimer
 func _ready():
 	currentTimer = CountdownMax
 	$HUD/Countdown.text = str(currentTimer)
-
-
+	
+	while currentTimer > 0:
+		yield(get_tree().create_timer(1.0), "timeout")
+		currentTimer = currentTimer - 1
+		print(currentTimer)
+	print("Game Over")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
