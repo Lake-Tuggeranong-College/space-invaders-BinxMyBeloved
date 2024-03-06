69433521e57b46cf86801406a3f368773464d79f
BinxMyBeloved <159493132+BinxMyBeloved@users.noreply.github.com>
2024-03-06 12:59:20 +1100

    current score and level end
    ending level redirects to main menu

--- MainGame/MainGame.gd
+++ MainGame/MainGame.gd
@@ -1,18 +1,24 @@
 extends Control
 
-export(int) var CountdownMax
+export(int) var CountdownMax = 90
 var currentTimer
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	currentTimer = CountdownMax
 	$HUD/Countdown.text = str(currentTimer)
-	
 	while currentTimer > 0:
 		yield(get_tree().create_timer(1.0), "timeout")
+		$HUD/Countdown.text = str(currentTimer)
 		currentTimer = currentTimer - 1
 		print(currentTimer)
 	print("Game Over")
+func _process(delta):
+	$HUD/CurrentScore.text = str(GlobalVariables.scoringInformation["currentScore"])
+	if "1600" in str(GlobalVariables.scoringInformation["currentScore"]) : 
+		print("Level End")
+		get_tree().change_scene("res://Menu/Menu.tscn")
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
+
