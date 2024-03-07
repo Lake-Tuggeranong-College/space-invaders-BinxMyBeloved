9485e2bfb0aa9e9893b79428b048f018faee5393
BinxMyBeloved <159493132+BinxMyBeloved@users.noreply.github.com>
2024-03-08 09:12:40 +1100

    working on win screen

--- Global.gd
+++ Global.gd
@@ -5,6 +5,6 @@ var enemyBulletInstanceCount = 0
 var scoringInformation = {
 	"currentScore": 0,
 	"currentPlayer": "User",
-	"highScore": 0,
+	"highScore": [0,0,0],
 	"highScorePlayersName" : "Winner"
 }
