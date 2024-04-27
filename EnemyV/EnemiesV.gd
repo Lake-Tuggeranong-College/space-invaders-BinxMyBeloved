789433b08b9d999504582af5fed1b2988dbf56b3
BinxMyBeloved <8041314@schoolsnet.act.edu.au>
2024-04-27 13:38:06 +1000

    trying to add 3rd enemy type

---
+++ EnemyV/EnemiesV.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+var speed = 10
+
+func _ready():
+	set_physics_process(true)
+
+func _physics_process(delta):
+	global_position.y += speed * delta
+	pass
+	#move_and_collide(Vector2(speed * delta, 0))
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
