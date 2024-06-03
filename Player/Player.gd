bd113d10dc25240a8f0273702cf797dbd70aa269
BinxMyBeloved <159493132+BinxMyBeloved@users.noreply.github.com>
2024-06-03 13:02:33 +1000

    fix movement

--- Player/Player.gd
+++ Player/Player.gd
@@ -8,14 +8,13 @@ func _ready():
 	set_physics_process(true)
 	if GlobalVariables.scoringInformation["totalScore"] <= 2:
 		reduceHealth()
+	if GlobalVariables.scoringInformation["totalScore"] >= 3:
+		movement_speed = 300
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if GlobalVariables.scoringInformation["totalScore"] >= 3:
-		movement_speed = 300
-	if GlobalVariables.scoringInformation["totalScore"] <= 3:
-		# This was causing the players health to drop extremely quickly.
-		#reduceHealth()
+	if health == 0:
+		get_tree().change_scene("res://LoseScreen/LoseScreen.tscn")
 		pass
 	if Input.is_action_just_pressed("fire"):
 		if GlobalVariables.bulletInstanceCount < 3:
@@ -30,12 +29,12 @@ func _physics_process(delta):
 	if Input.is_action_pressed("ui_right"):
 		if position.x < 1250:
 			move_and_collide(Vector2(movement_speed * delta, 0))
-	if Input. is_action_pressed("ui_up"):
-		if position.y > 10:
-			move_and_collide(Vector2(0, -movement_speed * delta))
-	if Input. is_action_pressed("ui_down"):
-		if position.y < 640:
-			move_and_collide(Vector2(0, movement_speed * delta))
+	#if Input. is_action_pressed("ui_up"):
+		#if position.y > 10:
+			#move_and_collide(Vector2(0, -movement_speed * delta))
+	#if Input. is_action_pressed("ui_down"):
+		#if position.y < 640:
+			#move_and_collide(Vector2(0, movement_speed * delta))
 	if health == 0:
 		get_tree().change_scene("res://LoseScreen/LoseScreen.tscn")
 
